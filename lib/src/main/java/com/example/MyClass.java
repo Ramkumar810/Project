0f8f3394aec2b9c3150893c5ee931c7744b4d6b5
Ramkumar810 <ramkumar.perumal@adyog.com>
2017-05-16 14:47:13 +0530

    Initial Commit

---
+++ lib/src/main/java/com/example/MyClass.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import sun.rmi.runtime.Log;
+
+public class MyClass {
+    public void interFunc(){
+        System.out.print("HelloWorld");
+    }
+
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.example.mylibrary;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by kannas on 5/15/2017.
+ */
+
+public class HelloWorld extends AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_hello);
+
+    }
+    public void helloWorldFunction(){
+       // Toast.makeText(this,"helloWorldFunction is executed",Toast.LENGTH_SHORT).show();
+        Log.d("name","helloWorldFunction is executed");
+    }
+}
